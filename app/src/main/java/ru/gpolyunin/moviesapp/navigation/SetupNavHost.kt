459b03e016bdff17460193f5e17ec87d55c34ba4
Georgy Polyunin <ryenjack33@gmail.com>
2022-12-16 22:57:08 +0300

    Lesson_5

--- app/src/main/java/ru/gpolyunin/moviesapp/navigation/SetupNavHost.kt
+++ app/src/main/java/ru/gpolyunin/moviesapp/navigation/SetupNavHost.kt
@@ -29,7 +29,7 @@ fun setupNavHost(navController: NavHostController, viewModel: MainViewModel) {
             mainScreen(navController = navController, viewModel = viewModel)
         }
         composable(route = Screens.Details.route + "/{Id}") { backStackEntry ->
-            DetailsScreen(navController = navController, viewModel = viewModel, itemId = backStackEntry.arguments?.getString("Id")?: "1")
+            DetailsScreen(viewModel = viewModel, itemId = backStackEntry.arguments?.getString("Id")?: "1")
         }
     }
 }
